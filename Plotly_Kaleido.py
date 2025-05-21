5a9176e1c8c1bbe7de1abc4f135e0b9a906ca930
zbpgmy <Pavel.Egerle@onsemi.com>
2025-05-21 10:32:07 +0200

    Plotly_Kaleido

---
+++ Plotly_Kaleido.py
@@ -0,0 +1,15 @@
+﻿# import plotly.express as px
+
+# fig = px.scatter(x=[1, 2, 3], y=[4, 5, 6], title="Test")
+# fig.write_image("test.png")
+# print("Obrázek uložen.")
+
+# import kaleido
+# print("Kaleido funguje.")
+
+import plotly.express as px
+import plotly.io as pio
+
+fig = px.scatter(x=[1, 2, 3], y=[4, 5, 6], title="Test")
+pio.write_image(fig, "test.png", format="png", scale=2)
+print("Obrázek uložen.")
